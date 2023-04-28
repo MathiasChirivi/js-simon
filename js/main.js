8a4281339ced87915521db24ca69188db227c927
MathiasChirivi <chirivimathias3@gmail.com>
2023-04-28 15:59:41 +0200

    generazione 5 numeri casuali

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log("Benvenuto!!");
+//console.log("Benvenuto!!");
+
+numeriCasuali = document.getElementById("numeriCasuali");
+
+let number = [];
+getRandomNumber(0,100);
+
+document.getElementById("numeriCasuali").innerHTML = number;
+
+
+function getRandomNumber(min, max) {
+    for (let i = 0; i < 5; i++) {
+        min = parseInt(min);
+        max = parseInt(max);
+        let numeroCasuale = Math.floor(Math.random() * ((max + 1) - min) + min);
+        if (!number.includes(numeroCasuale)) {
+            number.push(numeroCasuale);
+        }
+        console.log(numeroCasuale);
+    }   
+}
