bbb0367af06893545ddf04dfc46306041bba3412
MathiasChirivi <chirivimathias3@gmail.com>
2023-04-30 21:54:28 +0200

    fix code with resulte on HTML

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 //console.log("Benvenuto!!");
-
+/*
+*/
 numeriCasuali = document.getElementById("numeriCasuali");
+btnStart = document.getElementsByClassName("start");
+numeriIndoviati = document.getElementById("NumeriIndovinati");
+resulte = document.getElementById("risultato")
 
 let number = [];
 getRandomNumber(0,100);
@@ -23,7 +27,7 @@ function getRandomNumber(min, max) {
 
 //Countdown di 30 secondi e poi rimuove la stringa stampata 
  let secondi = 3;
- alert("Ora partirà un timer di 30 secondi, e devi riuscire a ricordare quanti piu numeri possibili")
+ alert("Ora partirà un timer di 30 secondi, e devi riuscire a ricordare quanti piu numeri possibili");
  let timer = setInterval(function() { 
 
      if(secondi == 0) {
@@ -57,13 +61,13 @@ function chiediNumeri(){
         console.log("FAI PENA");
     } 
     if (numeriInseritiUtente.length == 1){
-        alert("SEI SCARSO NE HAI INDOVINATO SOLO 1", numeriInseritiUtente.length);
+        alert("SEI SCARSO NE HAI INDOVINATO SOLO 1", numeriInseritiUtente);
     } 
     if (numeriInseritiUtente.length == 2){
         alert("SEI MODESTAMENTE SCARSO NE HAI INDOVINATI SOLO 2", numeriInseritiUtente.length);
     } 
     if (numeriInseritiUtente.length == 3){
-        alert("SEI UN PRINCIPIANRE NE HAI INDOVINATO SOLO 3", numeriInseritiUtente.length);
+        alert("SEI UN PRINCIPIANTE NE HAI INDOVINATO SOLO 3", numeriInseritiUtente.length);
     } 
     if (numeriInseritiUtente.length == 4){
         alert("CI SEI QUASI NE HAI INDOVINATO 4", numeriInseritiUtente.length);
@@ -73,4 +77,42 @@ function chiediNumeri(){
     }
     
 }
+/*
+numeriCasuali = document.getElementById("numeriCasuali");
+btnStart = document.getElementById("start");
+numeriIndoviati = document.getElementById("NumeriIndovinati");
+resulte = document.getElementById("risultato");
+
+let number = [];
+
+btnStart.addEventListener(`click`, function () {
+    
+    console.log("Gioco Avviato");
+    getRandomNumber(0,100);
+    
+    document.getElementById("start").style.display = "none";
+    
+    console.log("I numeri da ricordare sono : ", number);
+    
+    document.getElementById("numeriCasuali").innerHTML = number;
+    
+    let secondi = 3;
+    alert("Ora partirà un timer di 30 secondi, e devi riuscire a ricordare quanti piu numeri possibili");
+})
+
+function getRandomNumber(min, max) {
+    for (let i = 0; i < 5; i++) {
+        min = parseInt(min);
+        max = parseInt(max);
+        let numeroCasuale = Math.floor(Math.random() * ((max + 1) - min) + min);
+        if (!number.includes(numeroCasuale)) {
+            number.push(numeroCasuale);
+        }
+        console.log(numeroCasuale);
+    }   
+}
+*/
+
+
+
 
